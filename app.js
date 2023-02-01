018d8ac7420a233d6b5d0f172ee914bf1ac28137
xPapashi <dtomkowski1@gmail.com>
2023-02-01 22:38:44 +0000

    Add basic outline of calculator and some funcs Added calculator made in css with grids. Introduced functions for add, subtract, divide etc.

--- app.js
+++ app.js
@@ -1,1 +1,31 @@
-console.log('Hello World!');
+const display = document.querySelector("#screen-display");
+
+function add(num1, num2) {
+  return num1 + num2;
+}
+
+function subract(num1, num2) {
+  return num1 - num2;
+}
+
+function multiply(num1, num2) {
+  return num1 * num2;
+}
+
+function divide(num1, num2) {
+  return num1 / num2;
+}
+
+function operate(operator) {
+  return operator;
+}
+
+function changeScreen(element, text) {
+  return (element.innerText = text);
+}
+
+function calculate() {
+  changeScreen(display, operate(subract(10, 2)));
+}
+
+calculate();
