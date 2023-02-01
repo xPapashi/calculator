a61523086d2c2f8c5c05055cf87855b129253e1e
xPapashi <dtomkowski1@gmail.com>
2023-02-01 23:35:22 +0000

    Add button press for numbers Added function where a number is pressed it will be displayed

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const display = document.querySelector("#screen-display");
+let displayValue = Number(display.innerText);
 
 function add(num1, num2) {
   return num1 + num2;
@@ -24,8 +25,49 @@ function changeScreen(element, text) {
   return (element.innerText = text);
 }
 
-function calculate() {
-  changeScreen(display, operate(subract(10, 2)));
+function getButtons() {
+  const buttons = document.querySelectorAll(".grid-item");
+
+  buttons.forEach((button) => {
+    button.addEventListener("click", (e) => {
+      switch (e.target.innerText) {
+        case "1":
+          display.innerText = "1";
+          break;
+        case "2":
+          display.innerText = "2";
+          break;
+        case "3":
+          display.innerText = "3";
+          break;
+        case "4":
+          display.innerText = "4";
+          break;
+        case "5":
+          display.innerText = "5";
+          break;
+        case "6":
+          display.innerText = "6";
+          break;
+        case "7":
+          display.innerText = "7";
+          break;
+        case "8":
+          display.innerText = "8";
+          break;
+        case "9":
+          display.innerText = "9";
+          break;
+        case "0":
+          display.innerText = "0";
+          break;
+      }
+    });
+  });
 }
 
+function calculate() {
+  //   changeScreen(display, operate(subract(10, 2)));
+}
 calculate();
+getButtons();
